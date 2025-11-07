99920bc5ef115f31c1ec735f8dfbd7df619cb5aa
jaddu17 <shafajaddu@gmail.com>
2025-11-07 14:29:44 +0700

    membuat nav host

--- app/src/main/java/com/example/pertemuan6/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan6/Navigasi.kt
@@ -20,4 +20,35 @@ enum class Navigasi{
 @Composable
 fun DataApp(
     navController: NavHostController = rememberNavController()
-)
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian(
+                    //pilihanJK = JenisK.map {id -> konteks.resources.getString(id)},
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavController
+){
+    navController.popBackStack(Navigasi.Formulirku.name, inclusive = false)
+}
