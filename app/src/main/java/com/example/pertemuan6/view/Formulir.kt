a8873a0d6f6b15a587b3946c68f481733257d27d
jaddu17 <shafajaddu@gmail.com>
2025-11-07 10:24:32 +0700

    memberi divider dan membuat radio button

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -55,5 +55,22 @@ fun FormIsian(
                     label = {Text(text = "Nama Lengkap")},
                     onValueChange = {},
                 )
+                HorizontalDivider(modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(250.dp),
+                    thickness = Thickness,color = Color.Red)
+                Row{
+                    jenisK.forEach{
+                        item ->
+                        Row(verticalAlignment = Alignment.CenterVertically){
+                            RadioButton(
+                                selected = false,
+                                onClick = {item}
+                            )
+                            Text(text = item)
+                        }
+                    }
+                }
+
     }
 }
